ddbda0da82f45ce92a8493f7fe8b8a7186068c86
rash <orashusedmund@gmail.com>
2024-02-12 13:20:37 +0100

    .

--- app/_core/ui/guards/path-wrapper/path-wrapper.tsx
+++ app/_core/ui/guards/path-wrapper/path-wrapper.tsx
@@ -8,7 +8,7 @@
 import type { IDocument } from '@/interfaces/entities';
 
 export default function PathWrapper(Component: React.JSXElementConstructor<any>) {
-  return function Gaurd(props: any) {
+  return function Guard(props: any) {
     const content: IDocument[] = [];
 
     return <Component {...props} content={content} />;
