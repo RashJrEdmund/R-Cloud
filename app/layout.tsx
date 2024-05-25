a0a1689ccf73e449042fd237df9520b61400de94
rash <orashusedmund@gmail.com>
2024-05-25 12:11:12 +0100

    updated twitter specific description in meta data export

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { NavBar } from '@/components/molecules';
 import { ReactQueryClientProvider } from '@/features/react-query';
 import { Toaster } from 'sonner';
 
+const appDescription = 'Cloud storage web application. Upload, manage and download files';
+
 export const metadata: Metadata = {
   title: {
     template: '%s | R - Cloud',
@@ -22,10 +24,28 @@ export const metadata: Metadata = {
   ],
   openGraph: {
     type: 'website',
-    description: 'Cloud storage web application. Upload, manage and download files',
+    description: appDescription,
     siteName: 'R - cloud',
-    title: 'R - cloud',
+    title: {
+      template: '%s | R - Cloud',
+      default: 'R - Cloud',
+    },
   },
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'R - Cloud',
+    // startupImage: []
+  },
+  twitter: {
+    card: 'summary',
+    creator: 'orashus',
+    description: appDescription,
+    title: {
+      template: '%s | R - Cloud',
+      default: 'R - Cloud',
+    },
+  }
 };
 
 interface Props {
